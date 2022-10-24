2fddfed62d561c3b733805fa62330dab281c57ce
Anabilgg <nabil.dakkoune.86@hotmail.com>
2022-10-24 14:09:19 +0200

    Commit #1

--- js/script.js
+++ js/script.js
@@ -4,16 +4,17 @@
 import * as THREE from 'https://unpkg.com/three@0.126.1/build/three.module.js';
 import { OrbitControls } from 'https://unpkg.com/three@0.126.1/examples/jsm/controls/OrbitControls.js';
 
-
 const fov = 90;
 const aspect = window.innerWidth / window.innerHeight; // i.e : 1920/1080
 const near = 1.0;
 const far = 1000;
-const rotationSpeed = 0.005;
+const rotationSpeed = 0.0002;
 const size = 3;
+var MAX_X, MAX_Y, MAX_Z;
+var MAX_VECTOR;
 
-var camera, scene, renderer, controls;
-// var icosahedrons = [];
+var camera, scene, renderer, controls, raycaster, mousePosition, intersects, highlightCube;
+var icosahedronsAndLines = [];
 
 init();
 animate();
@@ -25,17 +26,68 @@ function init(){
 
     scene = new THREE.Scene();
 
-    initMap( 2, 2, 2 );
 
-    var object;
+    initHighlightCube();
+    initMap( 2, 5, 4 );
 
-    object = placeObject( 1, 1, 1, 0xff0000 );
+    
+
+   placeObject( 1, 2, 1, 0xff0000 );
+
+    mousePosition = new THREE.Vector2();
+    raycaster = new THREE.Raycaster();
 
     renderer = new THREE.WebGLRenderer();
     renderer.setSize( window.innerWidth, window.innerHeight );
     document.body.appendChild( renderer.domElement );
 
     controls = new OrbitControls(camera, renderer.domElement);
+
+    document.addEventListener( 'mousemove', onMouseMove );
+
+    window.addEventListener( 'resize', onWindowResize );
+
+}
+
+function initHighlightCube() {
+    const geometry = new THREE.BoxGeometry( size, size, size );
+    const material = new THREE.MeshBasicMaterial( {
+         side : THREE.DoubleSide
+         } );
+    highlightCube = new THREE.Mesh( geometry, material );
+    scene.add( highlightCube );
+
+    highlightCube.position.set( size/2, size/2, size/2 );
+
+}
+
+function onWindowResize() {
+
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+
+    renderer.setSize( window.innerWidth, window.innerHeight );
+
+}
+
+function onMouseMove( event ) {
+
+    mousePosition.x = ( event.clientX / window.innerWidth ) * 2 - 1;
+    mousePosition.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
+    raycaster.setFromCamera(mousePosition, camera);
+    intersects = raycaster.intersectObjects(scene.children);
+    
+    intersects.forEach( function(intersect) {
+        if(intersect.object.isLineSegments){
+            
+            //console.log(intersect);
+            const highlightPos = new THREE.Vector3().copy(intersect.point).min(MAX_VECTOR).floor();
+            console.log(highlightPos)
+            highlightCube.position.set( size/2 + highlightPos.x, size/2 + highlightPos.y, size/2 + highlightPos.z );
+
+        }
+     })
+
 }
 
 function animate() {
@@ -49,16 +101,14 @@ function animate() {
 
 function render() {
     
-    scene.traverse( function ( object ) {
+    scene.traverse( function () {
 
-        if ( object.isMesh === true ) {
+        icosahedronsAndLines.forEach( function(element) {
+            element.rotation.z += rotationSpeed;
+	        element.rotation.y += rotationSpeed;
+        })
 
-            object.rotation.z += rotationSpeed;
-	        object.rotation.y += rotationSpeed;
-
-        }
-
-    } );
+     } );
 
     renderer.render( scene, camera );
 
@@ -68,7 +118,8 @@ function placeInvisibleCube( x, y, z ){
 
     const geometry = new THREE.BoxGeometry( size, size, size );
     const material = new THREE.MeshBasicMaterial( {
-         visible : false
+         visible : false,
+         side : THREE.DoubleSide
          } );
     const boxgeometry = new THREE.Mesh( geometry, material );
     scene.add( boxgeometry );
@@ -87,19 +138,20 @@ function placeObject( x, y, z, color){
     const geometry = new THREE.IcosahedronGeometry( 1, 1 );
     const material = new THREE.MeshBasicMaterial( { 
         color: color,
-        wireframe : true
+//        wireframe : true
      } );
     const icosahedron = new THREE.Mesh( geometry, material );
 
     scene.add( icosahedron );
-    icosahedron.position.set( x*size/2 , y*size/2 , z*size/2 );
+    icosahedron.position.set( size/2 + (x-1)*size , size/2 + (y-1)*size , size/2 + (z-1)*size );
 
-    // icosahedrons.push(icosahedron);
+    const edges = new THREE.EdgesGeometry( geometry );
+    const line = new THREE.LineSegments( edges, new THREE.LineBasicMaterial( { color: 0xffffff } ) );
+    scene.add( line );
+    line.position.set( size/2 + (x-1)*size , size/2 + (y-1)*size , size/2 + (z-1)*size );
 
-    // const edges = new THREE.EdgesGeometry( geometry );
-    // const line = new THREE.LineSegments( edges, new THREE.LineBasicMaterial( { color: 0xffffff } ) );
-    // scene.add( line );
-    // line.position.set( x*size/2 , y*size/2 , z*size/2 );
+    icosahedronsAndLines.push(icosahedron);
+    icosahedronsAndLines.push(line);
 
 }
 
@@ -111,6 +163,12 @@ function initMap( length, width, depth ){
             }
         }
     }
+
+    MAX_X = size*(width-1);
+    MAX_Y = size*(length-1);
+    MAX_Z = size*(depth-1);
+
+    MAX_VECTOR = new THREE.Vector3( MAX_X, MAX_Y, MAX_Z );
 }
 
 
